245af61ea48c80e3ac3a562173261abe47ea83b5
ly1997yes <39435810+ly1997yes@users.noreply.github.com>
2018-09-27 20:39:49 +0800

    提交

---
+++ src/main/java/pers/shayz/crud/service/EmployeeService.java
@@ -0,0 +1,55 @@
+package pers.shayz.crud.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pers.shayz.crud.bean.Employee;
+import pers.shayz.crud.bean.EmployeeExample;
+import pers.shayz.crud.dao.EmployeeMapper;
+
+import java.util.List;
+
+/**
+ * @author ZhouXiaoyu
+ */
+@Service
+public class EmployeeService {
+
+    @Autowired
+    EmployeeMapper employeeMapper;
+
+    public List<Employee> getAll() {
+        return employeeMapper.selectByExampleWithDept(null);
+    }
+
+    public void saveEmp(Employee employee) {
+        employeeMapper.insertSelective(employee);
+    }
+
+    public boolean checkUser(String empName) {
+        EmployeeExample example = new EmployeeExample();
+        EmployeeExample.Criteria criteria = example.createCriteria();
+        criteria.andEmpNameEqualTo(empName);
+        long count = employeeMapper.countByExample(example);
+        return count == 0;
+    }
+
+    public Employee getEmp(Integer id){
+        Employee employee = employeeMapper.selectByPrimaryKey(id);
+        return employee;
+    }
+
+    public void updateEmp(Employee employee) {
+        employeeMapper.updateByPrimaryKeySelective(employee);
+    }
+
+    public void deleteEmp(Integer id) {
+        employeeMapper.deleteByPrimaryKey(id);
+    }
+
+    public void deleteBatch(List<Integer> ids) {
+        EmployeeExample example = new EmployeeExample();
+        EmployeeExample.Criteria criteria = example.createCriteria();
+        criteria.andEmpIdIn(ids);
+        employeeMapper.deleteByExample(example);
+    }
+}
